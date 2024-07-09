33e0fc240c1901e09c11e89f7ec8c95d83b4ca97
Jegs01 <bodcafe@gmail.com>
2024-07-09 15:02:23 +0100

    Rectify errors

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -56,23 +56,6 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
-    const lazyImages = document.querySelectorAll("img.lazy");
-
-    const imageObserver = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const img = entry.target;
-                img.src = img.dataset.src;
-                img.classList.remove("lazy");
-                observer.unobserve(img);
-            }
-        });
-    });
-
-    lazyImages.forEach(image => {
-        imageObserver.observe(image);
-    });
-
     // Display visit message
     const visitMessage = document.getElementById("visit-message");
     const lastVisit = localStorage.getItem("lastVisit");
